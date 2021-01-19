427b87ea9d32faa51626956237deb06cd170f165
ViolettaSchmalzel <violetta.schmalzel@informatik.hs-fulda.de>
2021-01-19 22:17:08 +0100

    Add F

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
     System.out.println("Katze ist schwarz");
 
     System.out.println("Das ist das Feature B");
-    System.out.println("ABCDE");
+    System.out.println("ABCDEF");
 
 
   }
